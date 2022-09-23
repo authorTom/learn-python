aab42f8cb2f3bc29fc2c04d51c887337bb9a8cd3
authorTom <thom@swright.net>
2022-09-23 19:29:56 +0100

    new commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+message = "Hello World" # defining a variable , using a string
+print(message)
+
+# Anything in a quote is considered a string
+name = "harry hill"
+print (name.title()) # this will make the first letters of the word in capitals
+print (name.upper()) # all lowercase
+print (name.lower()) # all uppercase
+
+# f-strings - f is for format
+first_name="father"
+second_name="christmas"
+name = f"{first_name} {second_name}" #we can use an f-string to combine variables
+print(name)
+print(f"Hello, {first_name.title()}")
+
